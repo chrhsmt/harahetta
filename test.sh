f084c7db72b882c2c4459bcb162c3c7ec6af2cec
chrhsmt <chr@chrhsmt.com>
2015-05-26 20:26:18 +0900

    Change test shell script

--- test.sh
+++ test.sh
@@ -6,12 +6,21 @@
   sleep 1;
   echo User-Agent: harahetta-client-1.0;
   sleep 1;
-  echo location-type: zip;
+  echo location-type: address,station;
   sleep 1;
-  echo location: 1000001;
+  echo location: 五反田;
   sleep 1;
   echo ; # End of Header
   sleep 1;
   echo ;
-  sleep 1;
+  sleep 3 ;
 ) | telnet #localhost 8810
+
+
+  # echo location-type: geographic;
+  # sleep 1;
+  # echo location: 35.669220,139.761457;
+
+  # echo location-type: phone;
+  # sleep 1;
+  # echo location: 0354376500;
